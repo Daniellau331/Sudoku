874e2c31a60133e7d649d21c074ca4047a460cea
Daniellau331 <daniellau331@gmail.com>
2019-12-19 22:26:49 -0800

    created two classes: Grid and Cube

--- GUI.py
+++ GUI.py
@@ -1,2 +1,30 @@
-#
+# GUI.py
 import pygame
+
+
+class Grid:
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+
+class Cube:
+    rows =9
+    cols =9
+
+    def __init__(self,value, row,col,width,height):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+
